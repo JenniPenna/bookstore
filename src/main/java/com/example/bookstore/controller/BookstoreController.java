e471b7f44a44d1a99b6e0a92e4f528ce48b9be01
bhq654@myy.haaga-helia.fi <bhq654@myy.haaga-helia.fi>
2025-09-04 16:43:38 +0300

    delete, addbook, edit ja category lisätty

--- src/main/java/com/example/bookstore/controller/BookstoreController.java
+++ src/main/java/com/example/bookstore/controller/BookstoreController.java
@@ -2,19 +2,55 @@ package com.example.bookstore.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.bookstore.domain.Book;
 import com.example.bookstore.domain.BookRepository;
 
 @Controller
 public class BookstoreController {
     private BookRepository repository;
+
     public BookstoreController(BookRepository repository) {
         this.repository = repository;
     }
 
- @RequestMapping(value= {"/", "/booklist"})
-    public String bookList(Model model) {	
+    @RequestMapping(value = { "/", "/booklist" })
+    public String bookList(Model model) {
         model.addAttribute("books", repository.findAll());
         return "booklist";
+    }
+
+    // ADD: Show add book form
+    @RequestMapping(value = "/addbook")
+    public String addbook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    // SAVE: Save new book
+    @PostMapping("/save")
+    public String save(Book book) {
+        repository.save(book);
+        return "redirect:/booklist";
+    }
+
+    // DELETE: Delete book by id
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+        repository.deleteById(bookId);
+        return "redirect:../booklist";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editBook(@PathVariable("id") Long bookId, Model model) {
+        Book book = repository.findById(bookId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid book Id:" + bookId));
+        model.addAttribute("book", book);
+        return "editbook"; // viittaa uuteen Thymeleaf-sivuun
     }
 }
